455645b1effc0da9c1b64d1161381ec2057993b6
Lautaro <lautaroSilva9500@gmail.com>
2019-10-17 09:19:29 -0300

    Doble workflow y reporte de errores
    Los test de unidad y de integracion se ejecutan en workflos distintos.
    Cada workflow genera un reporte independiente de cada test que fallo

--- e2e/protractor-ci.conf.js
+++ e2e/protractor-ci.conf.js
@@ -1,10 +1,23 @@
 const config = require('./protractor.conf').config;
+var jasmineReporters = require('jasmine-reporters');
 
 config.capabilities = {
-  browserName: 'chrome',
-  chromeOptions: {
-    args: ['--headless', '--no-sandbox', '--disable-gpu']
-  }
+    browserName: 'chrome',
+    chromeOptions: {
+        args: ['--headless', '--no-sandbox']
+    }
 };
+
+config.onPrepare = function() {
+  require('ts-node').register({
+    project: require('path').join(__dirname, './tsconfig.json')
+  });
+  var jasmineReporters = require('jasmine-reporters');
+  jasmine.getEnv().addReporter(new jasmineReporters.JUnitXmlReporter({
+    consolidateAll: true,
+    savePath: '../report',
+    filePrefix: 'test-result'
+  }));
+}
 
 exports.config = config;
